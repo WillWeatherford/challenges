d9d2b127e1a100b37d29caf138743630163932b3
Will Weatherford <weatherford.william@gmail.com>
2016-09-04 16:45:05 -0700

    Not really working attempt at divide and conquer strategy.

--- max-stock-profit/max_stock_profit.py
+++ max-stock-profit/max_stock_profit.py
@@ -30,10 +30,74 @@ def calc_profit(array):
     date, being the maximum difference possible from the given list.
     """
     enum = list(enumerate(array))
-    buy_idx, buy_val = min(enum, key=itemgetter(1))
-    sell_idx, sell_val = max(enum, key=itemgetter(1))
+    best_data = _recurse(enum, 0, len(enum) - 1)
 
-    if not sell_idx > buy_idx:
-        return 0, 0, 0
+    return best_data['buy_idx'], best_data['sell_idx'], best_data['profit']
 
-    return buy_idx, sell_idx, sell_val - buy_val
+
+def _recurse(enum, first_idx, last_idx):
+    min_idx, min_val = min((enum[first_idx], enum[last_idx]), key=itemgetter(1))
+    max_idx, max_val = max((enum[first_idx], enum[last_idx]), key=itemgetter(1))
+    buy_idx, buy_val = enum[first_idx]
+    sell_idx, sell_val = enum[last_idx]
+
+    result = {
+        'min_idx': min_idx,
+        'max_idx': max_idx,
+        'min_val': min_val,
+        'max_val': max_val,
+        'buy_idx': buy_idx,
+        'sell_idx': sell_idx,
+        'buy_val': buy_val,
+        'sell_val': sell_val,
+        'profit': sell_val - buy_val,
+    }
+
+    if last_idx - first_idx <= 1:
+        return result
+
+    half_idx = ((last_idx + 1 - first_idx) // 2) + first_idx
+    first_half_data = _recurse(enum, first_idx, half_idx - 1)
+    last_half_data  = _recurse(enum, half_idx, last_idx)
+
+    result = _best_data(first_half_data, last_half_data)
+    # import pdb;pdb.set_trace()
+    return result
+
+
+def _best_data(first, last):
+    """Get best data from combining first half and second half of the list."""
+    combo = {
+        'buy_idx': first['buy_idx'],
+        'sell_idx': last['sell_idx'],
+        'buy_val': first['buy_val'],
+        'sell_val': last['sell_val'],
+        'profit': last['sell_val'] - first['buy_val'],
+    }
+    if last['max_val'] > first['min_val']:
+        combo['buy_val'] = combo['min_val'] = first['min_val']
+        combo['buy_idx'] = combo['min_idx'] = first['min_idx']
+        combo['sell_val'] = combo['max_val'] = first['max_val']
+        combo['sell_idx'] = combo['max_idx'] = first['max_idx']
+        combo['profit'] = combo['sell_val'] - combo['buy_val']
+    return max((first, last, combo), key=itemgetter('profit'))
+
+
+# Iterate through the list
+# save the min-so-far
+    # if max has already been found, save min-so-far in case a better max
+    # comes along
+# save the max-so-far
+#   if min has already been found, take max-so-far
+#   be able to overwrite max with lower max if lower min comes along
+
+
+def calc_profit_nsquared(array):
+    """O(n ** 2) version of calc_profit."""
+    best = (0, 0, 0)
+    for idx1, num1 in enumerate(array):
+        for idx2, num2 in enumerate(array[idx1 + 1:]):
+            profit = num2 - num1
+            if profit > best[2]:
+                best = (idx1, idx1 + 1 + idx2, profit)
+    return best
