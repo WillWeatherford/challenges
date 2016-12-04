68c8fb30754f2f433f50f7d02a1726493f79ffd0
Will Weatherford <weatherford.william@gmail.com>
2016-12-03 19:29:46 -0800

    Updated docs with time and space complexity.

--- boggle/boggle.py
+++ boggle/boggle.py
@@ -1,8 +1,21 @@
-"""Algorithm to solve a square boggle board of any size.
+"""Algorithm to solve the word game Boggle.
+
+Solution provides a set of all allowed words found on a square Boggle board of
+any size, by a recursive depth-first search. Per the rules of Boggle, words
+must be at least 3 letters long, and cannot use the same tile more than once.
 
 Access to the entire dictionary of allowed English words is provided by a Trie
 data structure.
 
+Time Complexity:
+In the worst case scenario, the algorithm will explore every possible 16-letter
+permutation of the board. This is gives O(n!) factorial time.
+
+Space Complexity:
+In order to obey the rule of not re-using the same tile to make a word, the
+recursive search algorithm creates a new set object tracking already-visited
+tiles every time it explores a new tile, meaning space complexity is also O(n!)
+factorial.
 """
 from __future__ import unicode_literals, division
 from itertools import product
@@ -23,21 +36,20 @@ def solve_boggle_board(board):
 
 
 def find_words(y, x, chars, visited, board, all_words):
-    """Return words found recursively from the starting position."""
-    found_words = set()
-    if not all_words.contains_prefix(chars):
-        return found_words
-
+    """Return words found recursively from the given y, x position."""
     if len(chars) >= MIN_WORD_LEN and all_words.contains(chars):
-        found_words.add(chars)
+        found_words = {chars}
+    else:
+        found_words = set()
 
     visited.add((y, x))
 
     for n_y, n_x, n_char in neighbors(y, x, board):
         if (n_y, n_x) in visited:
             continue
-
         n_chars = chars + n_char
+        if not all_words.contains_prefix(n_chars):
+            continue
         # Necessary to create multiple copies of the visited set, since
         # different recursive branches will traverse grid in different order
         n_visited = visited.copy()
@@ -55,7 +67,7 @@ def iter_board(board):
 
 
 def neighbors(y, x, board):
-    """Return generator of all coords of neighbors and char at that coord."""
+    """Return generator of all neighbor coords and chars of given coord."""
     size = len(board)
     mods = (-1, 0, 1)
     for y_mod, x_mod in product(mods, mods):
