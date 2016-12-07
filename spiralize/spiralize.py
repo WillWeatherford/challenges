75b869cec4788beb435f5d1584b12855abaa4b12
Will Weatherford <weatherford.william@gmail.com>
2016-12-06 20:30:43 -0800

    Working sequence generator using iterators.

--- spiralize/spiralize.py
+++ spiralize/spiralize.py
@@ -1,9 +1,15 @@
 """Code challenge: create a square array with a spiral from the given size."""
+from itertools import cycle
 
 
 def spiralize(size):
     """Return list of lists -- 2D grid -- with a spiral of 1s starting in topleft."""
 
 
-def generate_moves(size):
-    """."""
+def generate_sequence(size):
+    """Return a generator of the correct moves to make a spiral."""
+    for n, flag in zip(range(size - 1, 1, -1), cycle((0, 1))):
+        if flag:
+            yield n - 1
+        else:
+            yield n
--- spiralize/test_spiralize.py
+++ spiralize/test_spiralize.py
@@ -30,15 +30,21 @@ CASES = [
 
 SEQUENCE_CASES = [
     (3, [2]),
+    (4, [3, 1]),
     (5, [4, 2, 2]),
-    (8, [7, 5, 5, 3, 3, 1])
-
+    (8, [7, 5, 5, 3, 3, 1]),
 ]
 
-def 
 
-@pytest.mark.parametrize('size, result', CASES)
-def test_spiralize(size, result):
-    """Test that spiral of given size is created correctly."""
-    from spiralize import spiralize
-    assert spiralize(size) == result
+@pytest.mark.parametrize('size, sequence', SEQUENCE_CASES)
+def test_gen_sequence(size, sequence):
+    """Test that correct sequence to move around spiral is generated."""
+    from spiralize import generate_sequence
+    assert list(generate_sequence(size)) == sequence
+
+
+# @pytest.mark.parametrize('size, result', CASES)
+# def test_spiralize(size, result):
+#     """Test that spiral of given size is created correctly."""
+#     from spiralize import spiralize
+#     assert spiralize(size) == result
