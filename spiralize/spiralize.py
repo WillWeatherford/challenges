35af4cc497e6bdbd538ba866159e4a56e6fb9c4f
Will Weatherford <weatherford.william@gmail.com>
2016-12-06 20:56:09 -0800

    Using zip of moves and directions. Itertools FTW

--- spiralize/spiralize.py
+++ spiralize/spiralize.py
@@ -1,10 +1,23 @@
 """Code challenge: create a square array with a spiral from the given size."""
 from itertools import cycle, chain
 
+DIRECTIONS = [
+    lambda y, x: (y, x + 1),
+    lambda y, x: (y + 1, x),
+    lambda y, x: (y, x - 1),
+    lambda y, x: (y - 1, x),
+]
+
 
 def spiralize(size):
     """Return list of lists -- 2D grid -- with a spiral of 1s starting in topleft."""
     grid = initialize_grid(size)
+    seq = chain((size, size - 1), generate_sequence(size))
+    seq = zip(cycle(DIRECTIONS), seq)
+    y, x = 0, -1
+    for direction, num_moves in seq:
+        y, x = move_and_write(num_moves, direction, y, x, grid)
+
 
 
 def initialize_grid(size):
@@ -12,7 +25,7 @@ def initialize_grid(size):
     return [[0] * size for _ in range(size)]
 
 
-def move_and_write(moves, direction, y, x):
+def move_and_write(num_moves, direction, y, x, grid):
     """Move the given number of moves/direction starting from y, x, setting coords to 1."""
 
 
