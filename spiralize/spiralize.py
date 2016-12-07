9b7f0a253ac42ab1d1f707a79c0a3dae07ea9783
Will Weatherford <weatherford.william@gmail.com>
2016-12-06 20:38:11 -0800

    Functions to initalize grid.

--- spiralize/spiralize.py
+++ spiralize/spiralize.py
@@ -1,9 +1,19 @@
 """Code challenge: create a square array with a spiral from the given size."""
-from itertools import cycle
+from itertools import cycle, chain
 
 
 def spiralize(size):
     """Return list of lists -- 2D grid -- with a spiral of 1s starting in topleft."""
+    grid = initialize_grid(size)
+
+
+def initialize_grid(size):
+    """Create the initial grid with blank spaces."""
+    return [[0] * size for _ in range(size)]
+
+
+def move_and_write(moves, direction, y, x):
+    """Move the given number of moves/direction starting from y, x, setting coords to 1."""
 
 
 def generate_sequence(size):
